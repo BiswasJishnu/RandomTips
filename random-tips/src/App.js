7ddfd26684aadd5e6952b2af471acc28d6502cfd
BiswasJishnu <biswasjishnu24@gmail.com>
2021-01-01 10:34:39 +0530

    Added axios and created a componentDidMount method

--- random-tips/src/App.js
+++ random-tips/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   state = {
     tips: ""
+  }
+
+  componentDidMount() {
+    console.log('MOUNTED')
   }
 
   render() {
